c701d240ba06a105e003c2728e1a898319f283c5
AfiqNSE <afiq@nse.com.my>
2024-06-26 12:06:04 +0800

    create login page

---
+++ pages/login_page.py
@@ -0,0 +1,18 @@
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+
+class LoginPage:
+    def __init__(self, driver: webdriver.Remote):
+        self.driver = driver
+        
+    def enter_username(self):
+        pass    
+
+    def enter_password(self):
+        pass
+
+    def click_login(self):
+        self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.Button[@content-desc="Login"]').click()
+
+
