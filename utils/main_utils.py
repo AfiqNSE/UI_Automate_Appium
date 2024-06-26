e4508ec31a0209fc1b15e883cfb7ea82c9dc8491
AfiqNSE <afiq@nse.com.my>
2024-06-26 12:05:49 +0800

    create main utils file

---
+++ utils/main_utils.py
@@ -0,0 +1,18 @@
+
+class Utils:
+
+    # Testing data for search
+    Test_Search_logsheetNo = 'DD30010924'
+    Test_Search_docketNo = 'JD300130143'
+    
+    # Testing data for assign
+    Test_Assign_logsheetNo = 'DD30010985'
+    
+    # Testing data for report
+    Test_dateFrom = '9, Tuesday, January 9, 2024'
+    Test_dateTo = '30, Tuesday, April 30, 2024'
+
+    def cancelButton():
+        pass
+        #NOTE: This code is to use cancel button 
+        # self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.Button[@resource-id="com.nse.project.nse_driver_flutter_app:id/btnBarcodeCaptureCancel"]').click()
