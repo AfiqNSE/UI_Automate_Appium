39002df6f7000e32117a41117fff23755acfd6d1
AfiqNSE <afiq@nse.com.my>
2024-06-28 09:33:40 +0800

    Update test language

--- tests/test_language.py
+++ tests/test_language.py
@@ -3,13 +3,13 @@ from appium.options.android import UiAutomator2Options
 from appium import webdriver
 
 from config import Config
-from pages.language_page import StaffLanguagePage
+from pages.language_page import LanguagePage
 
-class TestStaffLanguage(unittest.TestCase):
+class TestLanguage(unittest.TestCase):
     def setUp(self):
         options = UiAutomator2Options().load_capabilities(Config.capabilities)
         self.driver = webdriver.Remote(Config.appium_server_url, options=options)
-        self.language_page = StaffLanguagePage(self.driver)
+        self.language_page = LanguagePage(self.driver)
 
     def test_staff_language(self):
         self.language_page.change_staff_language()
