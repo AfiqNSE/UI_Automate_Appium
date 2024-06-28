949fefa7724613d44de555320e534b20fbbc1b52
AfiqNSE <afiq@nse.com.my>
2024-06-28 09:33:15 +0800

    Update run test

--- run_test.py
+++ run_test.py
@@ -1,6 +1,6 @@
 import unittest
 from tests.test_login import TestLogin
-from tests.test_language import TestStaffLanguage
+from tests.test_language import TestLanguage
 from tests.test_longhaul import TestLonghaul
 from tests.test_redeem import TestApproveRedeem
 from tests.test_search import TestSearch
@@ -12,14 +12,14 @@ if __name__ == '__main__':
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
 
-    # suite.addTests(loader.loadTestsFromTestCase(TestLogin))
-    # suite.addTests(loader.loadTestsFromTestCase(TestLanguage))
-    # suite.addTests(loader.loadTestsFromTestCase(TestLonghaul))
-    # suite.addTests(loader.loadTestsFromTestCase(TestApproveRedeem))
+    suite.addTests(loader.loadTestsFromTestCase(TestLogin))
+    suite.addTests(loader.loadTestsFromTestCase(TestLanguage))
+    suite.addTests(loader.loadTestsFromTestCase(TestLonghaul))
+    suite.addTests(loader.loadTestsFromTestCase(TestApproveRedeem))
     suite.addTests(loader.loadTestsFromTestCase(TestSearch))
-    # suite.addTests(loader.loadTestsFromTestCase(TestAssignPoint))
-    # suite.addTests(loader.loadTestsFromTestCase(TestAnalytics))
-    # suite.addTests(loader.loadTestsFromTestCase(TestReport))
+    suite.addTests(loader.loadTestsFromTestCase(TestAssignPoint))
+    suite.addTests(loader.loadTestsFromTestCase(TestAnalytics))
+    suite.addTests(loader.loadTestsFromTestCase(TestReport))
  
     unittest.TextTestRunner(verbosity=2).run(suite)
 
