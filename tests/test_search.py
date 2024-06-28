bbe41a1f32661f600e58251e2913f0c016ebed6e
AfiqNSE <afiq@nse.com.my>
2024-06-28 17:44:32 +0800

    update test search

--- tests/test_search.py
+++ tests/test_search.py
@@ -20,60 +20,60 @@ class TestSearch(unittest.TestCase):
             raise ValueError("No logsheet number provided")
         self.search_page.insert_logsheet(Constant.SEARCH_LOGSHEETNO)
         
-    # def test_02_pod_photoDocket(self):
-    #     self.search_page.nav_search()
+    def test_02_pod_photoDocket(self):
+        self.search_page.nav_search()
         
-    #     if Constant.POD_DOCKETNO_PHOTO == '':
-    #         raise ValueError("No docket number provided")
+        if Constant.POD_DOCKETNO_PHOTO == '':
+            raise ValueError("No docket number provided")
         
-    #     self.search_page.insert_docket(Constant.POD_DOCKETNO_PHOTO)
-    #     self.utils.pod_photo()
+        self.search_page.insert_docket(Constant.POD_DOCKETNO_PHOTO)
+        self.utils.pod_photo()
         
-    # def test_03_pod_uploadDocket(self):
-    #     self.search_page.nav_search()  
+    def test_03_pod_uploadDocket(self):
+        self.search_page.nav_search()  
           
-    #     if Constant.POD_DOCKETNO_UPLOAD == '':
-    #         raise ValueError("No docket number provided")
+        if Constant.POD_DOCKETNO_UPLOAD == '':
+            raise ValueError("No docket number provided")
 
-    #     self.search_page.insert_docket(Constant.POD_DOCKETNO_UPLOAD)
-    #     self.utils.pod_upload()
+        self.search_page.insert_docket(Constant.POD_DOCKETNO_UPLOAD)
+        self.utils.pod_upload()
         
-    # def test_04_pod_signatureDocket(self):
-    #     self.search_page.nav_search()      
+    def test_04_pod_signatureDocket(self):
+        self.search_page.nav_search()      
           
-    #     if Constant.POD_DOCKETNO_SIGNATURE == '':
-    #         raise ValueError("No docket number provided")
+        if Constant.POD_DOCKETNO_SIGNATURE == '':
+            raise ValueError("No docket number provided")
         
-    #     self.search_page.insert_docket(Constant.POD_DOCKETNO_SIGNATURE)
-    #     self.utils.pod_signature()
+        self.search_page.insert_docket(Constant.POD_DOCKETNO_SIGNATURE)
+        self.utils.pod_signature()
 
-    # def test_05_fail_docket(self):
-    #     self.search_page.nav_search()
+    def test_05_fail_docket(self):
+        self.search_page.nav_search()
         
-    #     if Constant.FAIL_DOCKETNO == '':
-    #         raise ValueError("No docket number provided")
+        if Constant.FAIL_DOCKETNO == '':
+            raise ValueError("No docket number provided")
 
-    #     self.search_page.insert_docket(Constant.FAIL_DOCKETNO)
-    #     self.utils.nav_fail()
+        self.search_page.insert_docket(Constant.FAIL_DOCKETNO)
+        self.utils.nav_fail()
         
-    # def test_06_delay_docket(self):
-    #     self.search_page.nav_search()
+    def test_06_delay_docket(self):
+        self.search_page.nav_search()
         
-    #     if Constant.DELAY_DOCKETNO == '':
-    #         raise ValueError("No docket number provided")
+        if Constant.DELAY_DOCKETNO == '':
+            raise ValueError("No docket number provided")
 
-    #     self.search_page.insert_docket(Constant.DELAY_DOCKETNO)
-    #     self.utils.nav_delay()
+        self.search_page.insert_docket(Constant.DELAY_DOCKETNO)
+        self.utils.nav_delay()
         
-    # def test_07_additional(self):
-        # self.search_page.nav_search()
+    def test_07_additional(self):
+        self.search_page.nav_search()
         
-        # if Constant.POD_DOCKETNO_SIGNATURE == '':
-        #     raise ValueError("No docket number provided")
+        if Constant.POD_DOCKETNO_SIGNATURE == '':
+            raise ValueError("No docket number provided")
         
-        # self.search_page.insert_docket(Constant.POD_DOCKETNO_SIGNATURE)
-        # self.utils.nav_viewSignature()
-        # self.utils.nav_docketPreview()
+        self.search_page.insert_docket(Constant.POD_DOCKETNO_SIGNATURE)
+        self.utils.nav_viewSignature()
+        self.utils.nav_docketPreview()
         
 
 
