94051344640ec0046fdf4e2a6c3987b1dbaa1871
AfiqNSE <afiq@nse.com.my>
2024-06-29 10:25:10 +0800

    Update test search

--- tests/test_search.py
+++ tests/test_search.py
@@ -4,14 +4,14 @@ from appium import webdriver
 
 from config import Config
 from pages.staff_page import SearchPage
-from utils.main_utils import Utils, Constant
+from components.main_components import Constant, Components
 
 class TestSearch(unittest.TestCase):
     def setUp(self):
         options = UiAutomator2Options().load_capabilities(Config.capabilities)
         self.driver = webdriver.Remote(Config.appium_server_url, options=options)
         self.search_page = SearchPage(self.driver)
-        self.utils = Utils(self.driver)
+        self.component = Components(self.driver)
 
     def test_01_logsheet_search(self):
         self.search_page.nav_search()
@@ -27,7 +27,7 @@ class TestSearch(unittest.TestCase):
             raise ValueError("No docket number provided")
         
         self.search_page.insert_docket(Constant.POD_DOCKETNO_PHOTO)
-        self.utils.pod_photo()
+        self.component.pod_photo()
         
     def test_03_pod_uploadDocket(self):
         self.search_page.nav_search()  
@@ -36,7 +36,7 @@ class TestSearch(unittest.TestCase):
             raise ValueError("No docket number provided")
 
         self.search_page.insert_docket(Constant.POD_DOCKETNO_UPLOAD)
-        self.utils.pod_upload()
+        self.component.pod_upload()
         
     def test_04_pod_signatureDocket(self):
         self.search_page.nav_search()      
@@ -45,7 +45,7 @@ class TestSearch(unittest.TestCase):
             raise ValueError("No docket number provided")
         
         self.search_page.insert_docket(Constant.POD_DOCKETNO_SIGNATURE)
-        self.utils.pod_signature()
+        self.component.pod_signature()
 
     def test_05_fail_docket(self):
         self.search_page.nav_search()
@@ -54,7 +54,7 @@ class TestSearch(unittest.TestCase):
             raise ValueError("No docket number provided")
 
         self.search_page.insert_docket(Constant.FAIL_DOCKETNO)
-        self.utils.nav_fail()
+        self.component.nav_fail()
         
     def test_06_delay_docket(self):
         self.search_page.nav_search()
@@ -63,7 +63,7 @@ class TestSearch(unittest.TestCase):
             raise ValueError("No docket number provided")
 
         self.search_page.insert_docket(Constant.DELAY_DOCKETNO)
-        self.utils.nav_delay()
+        self.component.nav_delay()
         
     def test_07_additional(self):
         self.search_page.nav_search()
@@ -72,8 +72,8 @@ class TestSearch(unittest.TestCase):
             raise ValueError("No docket number provided")
         
         self.search_page.insert_docket(Constant.POD_DOCKETNO_SIGNATURE)
-        self.utils.nav_viewSignature()
-        self.utils.nav_docketPreview()
+        self.component.nav_viewSignature()
+        self.component.nav_docketPreview()
         
 
 
