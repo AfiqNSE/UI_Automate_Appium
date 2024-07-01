13e0fdc57380ac0dca2d6d01cee3393c173f6962
AfiqNSE <afiq@nse.com.my>
2024-07-01 17:08:07 +0800

    Update run test

--- run_test.py
+++ run_test.py
@@ -3,7 +3,7 @@ from config import Config
 from tests.test_analytics import TestStaffAnalytics
 from tests.test_approve import TestStaffApprove
 from tests.test_assign import TestStaffAssign
-from tests.test_language import TestStaffLanguage
+from tests.test_language import TestDriverLanguage, TestStaffLanguage
 from tests.test_login import TestDriverLogin, TestStaffLogin
 from tests.test_longhaul import TestDriverLonghaul, TestStaffLonghaul
 from tests.test_report import TestStaffReport
@@ -12,9 +12,10 @@ from tests.test_search import TestStaffSearch
 
 if __name__ == '__main__':
     loader = unittest.TestLoader()
+    staff_suite = unittest.TestSuite()
+    driver_suite = unittest.TestSuite()
 
     #Staff Test
-    staff_suite = unittest.TestSuite()
     staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffLogin))
     staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffLanguage))
     staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffLonghaul))
@@ -25,12 +26,12 @@ if __name__ == '__main__':
     staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffReport))
     
     #Driver Test
-    driver_suite = unittest.TestSuite()
     driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverLogin))
+    driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverLanguage))
     driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverLonghaul))
  
-    # unittest.TextTestRunner(Config.VERBOSITY).run(staff_suite)
-    unittest.TextTestRunner(Config.VERBOSITY).run(driver_suite)
+    unittest.TextTestRunner(Config.VERBOSITY).run(staff_suite)
+    unittest.TextTestRunner(verbosity=Config.VERBOSITY).run(driver_suite)
 
     print("Test Done")
 
