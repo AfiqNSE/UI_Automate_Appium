c6160d0ca5abf032e92fa29a5932b0b5d7031fad
AfiqNSE <afiq@nse.com.my>
2024-07-01 17:07:24 +0800

    Update test longhaul

--- tests/test_longhaul.py
+++ tests/test_longhaul.py
@@ -1,8 +1,8 @@
 import unittest
-from appium.options.android import UiAutomator2Options
 from appium import webdriver
-
 from config import Config
+from appium.options.android import UiAutomator2Options
+from components.main_component import Components
 from pages.longhaul_page import LonghaulPage
 
 
@@ -21,6 +21,10 @@ class TestDriverLonghaul(unittest.TestCase):
         options = UiAutomator2Options().load_capabilities(Config.capabilities)
         self.driver = webdriver.Remote(Config.appium_server_url, options=options)
         self.longhaul = LonghaulPage(self.driver)
+        self.component = Components(self.driver)
 
     def test_longhaul(self):
+        self.component.nav_sideBar()
         self.longhaul.nav_driver_longhaul()
+        self.longhaul.scan_longhaul()
+        self.longhaul.insert_longhaul()
