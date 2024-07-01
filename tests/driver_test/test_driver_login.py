c4d639611e98e1b24a3aa43d99f7757305e8275e
AfiqNSE <afiq@nse.com.my>
2024-07-01 14:32:48 +0800

    Update test driver login

---
+++ tests/driver_test/test_driver_login.py
@@ -0,0 +1,21 @@
+import unittest
+from appium.options.android import UiAutomator2Options
+from parameterized import parameterized
+from appium import webdriver
+
+from config import Config
+from pages.login_page import LoginPage
+
+#TODO: Find a way to combine test for driver and staff
+class TestDriverLogin(unittest.TestCase):
+    def test_login(self):
+        options = UiAutomator2Options().load_capabilities(Config.capabilities)
+        self.driver = webdriver.Remote(Config.appium_server_url, options=options)
+        self.login_page = LoginPage(self.driver)
+        
+        self.login_page.enter_driver_username()
+        self.login_page.enter_password()
+        self.login_page.click_login()
+    
+
+        
