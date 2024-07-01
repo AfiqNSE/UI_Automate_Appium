50b09d31527e940b01aa7f91abfe438654d78aea
AfiqNSE <afiq@nse.com.my>
2024-07-01 17:07:51 +0800

    update tes history

--- tests/test_history.py
+++ tests/test_history.py
@@ -2,9 +2,14 @@ import unittest
 from appium.options.android import UiAutomator2Options
 from appium import webdriver
 
+from components.main_component import Components
 from config import Config
 
 class TestDriverHistory(unittest.TestCase):
     def setUp(self):
         options = UiAutomator2Options().load_capabilities(Config.capabilities)
         self.driver = webdriver.Remote(Config.appium_server_url, options=options)
+        self.component = Components(self.driver)
+        
+    def test_history(self):
+        self.component.nav_sideBar()
