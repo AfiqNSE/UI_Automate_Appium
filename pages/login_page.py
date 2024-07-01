18caddbeee8d9575172c8c0a1e55d28058af68c1
AfiqNSE <afiq@nse.com.my>
2024-07-01 14:32:56 +0800

    Update login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,18 +1,25 @@
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 
-from components.main_components import Constant
+from components.main_component import Constant
 
 #Testing class for Login
 class LoginPage:
+    #defining constructor  
     def __init__(self, driver: webdriver.Remote):
         self.driver = driver
-        
-    def enter_username(self):
+
+    def enter_staff_username(self):
         textfield = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]')
         textfield.click()
-        textfield.send_keys(Constant.USERNAME)
-        self.driver.hide_keyboard()       
+        textfield.send_keys(Constant.STAFF_USERNAME)
+        self.driver.hide_keyboard()     
+        
+    def enter_driver_username(self):
+        textfield = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]')
+        textfield.click()
+        textfield.send_keys(Constant.DRIVER_USERNAME)
+        self.driver.hide_keyboard()   
         
     def enter_password(self):
         textfield = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[2]')
