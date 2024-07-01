7ae76d1bac01cdd710d18cfe1dfe77d3ac4b24b9
AfiqNSE <afiq@nse.com.my>
2024-07-01 17:09:13 +0800

    Update driver page

--- pages/driver_page.py
+++ pages/driver_page.py
@@ -12,21 +12,27 @@ class NotificationPage:
 class HistoryPage:
     def __init__(self, driver: webdriver.Remote):
         self.driver = driver
-
-
-#TODO: Create Analytics test module  
-class AnalyticsPage:
-    def __init__(self, driver: webdriver.Remote):
-        self.driver = driver
+        
+    def nav_history(self):
+        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "History").click()
+        self.driver.back()
 
 
 #TODO: Create Invalid IOD test module  
 class InvalidIODPage:
     def __init__(self, driver: webdriver.Remote):
         self.driver = driver
+    
+    def nav_invalid(self):
+        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "Invalid IOD").click()
+        self.driver.back()
 
 
 #TODO: Create Reward test module  
 class RewardPage:
     def __init__(self, driver: webdriver.Remote):
         self.driver = driver
+        
+    def nav_reward(self):
+        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "Reward").click()
+        self.driver.back()
