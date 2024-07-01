a6ca075175619fa6f85fc323b707b0abe1332672
AfiqNSE <afiq@nse.com.my>
2024-07-01 14:33:15 +0800

    create driver page module

---
+++ pages/driver.page.py
@@ -0,0 +1,38 @@
+from appium import webdriver
+
+from appium.webdriver.common.appiumby import AppiumBy
+
+#TODO: Create Notification test module  
+class NotificationPage:
+    def __init__(self, driver: webdriver.Remote):
+        self.driver = driver
+
+
+#TODO: Create Notification test module  
+class LonghaulPage:
+    def __init__(self, driver: webdriver.Remote):
+        self.driver = driver
+
+
+#TODO: Create History test module  
+class HistoryPage:
+    def __init__(self, driver: webdriver.Remote):
+        self.driver = driver
+
+
+#TODO: Create Analytics test module  
+class AnalyticsPage:
+    def __init__(self, driver: webdriver.Remote):
+        self.driver = driver
+
+
+#TODO: Create Invalid IOD test module  
+class InvalidIODPage:
+    def __init__(self, driver: webdriver.Remote):
+        self.driver = driver
+
+
+#TODO: Create Reward test module  
+class RewardPage:
+    def __init__(self, driver: webdriver.Remote):
+        self.driver = driver
