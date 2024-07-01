6b6dce3310d1c87463a9e10a77f8ebf07a16c84e
AfiqNSE <afiq@nse.com.my>
2024-07-01 17:07:15 +0800

    Update test reward

--- tests/test_reward.py
+++ tests/test_reward.py
@@ -2,9 +2,17 @@ import unittest
 from appium.options.android import UiAutomator2Options
 from appium import webdriver
 
+from components.main_component import Components
 from config import Config
+from pages.driver_page import RewardPage
 
 class TestDriverReward(unittest.TestCase):
     def setUp(self):
         options = UiAutomator2Options().load_capabilities(Config.capabilities)
         self.driver = webdriver.Remote(Config.appium_server_url, options=options)
+        self.reward = RewardPage(self.driver)
+        self.component = Components(self.driver)
+        
+    def test_reward(self):
+        self.component.nav_sideBar()
+        self.reward.nav_reward()
