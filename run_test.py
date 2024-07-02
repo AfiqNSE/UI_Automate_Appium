0b64ef73aef5208571066c4316ca1ccb81ad5261
AfiqNSE <afiq@nse.com.my>
2024-07-02 12:19:51 +0800

    Update run test

--- run_test.py
+++ run_test.py
@@ -3,11 +3,13 @@ from config import Config
 from tests.test_analytics import TestStaffAnalytics
 from tests.test_approve import TestStaffApprove
 from tests.test_assign import TestStaffAssign
-from tests.test_language import TestDriverLanguage, TestStaffLanguage
-from tests.test_login import TestDriverLogin, TestStaffLogin
-from tests.test_longhaul import TestDriverLonghaul, TestStaffLonghaul
+from tests.test_language import TestStaffLanguage, TestDriverLanguage
+from tests.test_login import  TestStaffLogin, TestDriverLogin
+from tests.test_longhaul import TestStaffLonghaul, TestDriverLonghaul
 from tests.test_report import TestStaffReport
 from tests.test_search import TestStaffSearch
+from tests.test_notification import TestNotification
+from tests.test_homepage import TestStaffHome, TestDriverHome
 
 
 if __name__ == '__main__':
@@ -17,6 +19,7 @@ if __name__ == '__main__':
 
     #Staff Test
     staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffLogin))
+    staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffHome))
     staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffLanguage))
     staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffLonghaul))
     staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffApprove))
@@ -27,10 +30,11 @@ if __name__ == '__main__':
     
     #Driver Test
     driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverLogin))
-    driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverLanguage))
+    driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverHome))
+    driver_suite.addTests(loader.loadTestsFromTestCase(TestNotification))
     driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverLonghaul))
  
-    unittest.TextTestRunner(Config.VERBOSITY).run(staff_suite)
+    # unittest.TextTestRunner(verbosity=Config.VERBOSITY).run(staff_suite)
     unittest.TextTestRunner(verbosity=Config.VERBOSITY).run(driver_suite)
 
     print("Test Done")
