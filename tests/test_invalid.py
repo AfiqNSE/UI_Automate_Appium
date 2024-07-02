f5ab3774254b74fb06778d58b1306aa832178700
AfiqNSE <afiq@nse.com.my>
2024-07-02 16:56:48 +0800

    update test invalid

--- tests/test_invalid.py
+++ tests/test_invalid.py
@@ -4,15 +4,19 @@ from appium import webdriver
 
 from components.main_component import Components
 from config import Config
+from pages.home_page import DriverHomePage
 from pages.invalid_page import InvalidIODPage
 
 class TestDriverInvalid(unittest.TestCase):
     def setUp(self):
         options = UiAutomator2Options().load_capabilities(Config.capabilities)
         self.driver = webdriver.Remote(Config.appium_server_url, options=options)
-        self.invalid = InvalidIODPage(self.driver)
+        self.invalid_page = InvalidIODPage(self.driver)
         self.component = Components(self.driver)
         
     def test_invalid(self):
         self.component.nav_sideBar()
-        self.invalid.nav_invalid()
+        self.invalid_page.nav_invalid()
+        self.invalid_page.load_invalidPage()
+
+      
