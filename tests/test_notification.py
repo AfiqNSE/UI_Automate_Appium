4d9bfdf41acc20af404a6d12f6823aac54f1413a
AfiqNSE <afiq@nse.com.my>
2024-07-02 12:16:35 +0800

    Create test notification

---
+++ tests/test_notification.py
@@ -0,0 +1,14 @@
+import unittest
+from appium.options.android import UiAutomator2Options
+from appium import webdriver
+from config import Config
+from pages.notification_page import NotificationPage
+
+class TestNotification(unittest.TestCase):
+    def test_notification(self):
+        options = UiAutomator2Options().load_capabilities(Config.capabilities)
+        self.driver = webdriver.Remote(Config.appium_server_url, options=options)
+        self.notification_page = NotificationPage(self.driver)
+        
+        self.notification_page.nav_notification()
+ 
