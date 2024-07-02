dcf9844547ddddabfa141f6eb3667ef128ac0604
AfiqNSE <afiq@nse.com.my>
2024-07-02 12:16:26 +0800

    Update test approve

--- tests/test_approve.py
+++ tests/test_approve.py
@@ -2,9 +2,8 @@ import unittest
 from appium.options.android import UiAutomator2Options
 from appium import webdriver
 
-
 from config import Config
-from pages.staff_page import ApproveRedeemPage
+from pages.approve_page import ApproveRedeemPage
 
 class TestStaffApprove(unittest.TestCase):
     def setUp(self):
