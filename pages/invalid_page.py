cf453887baeb5fba6f15106592cbee29b569f78f
AfiqNSE <afiq@nse.com.my>
2024-07-02 16:57:56 +0800

    Update invalid page

--- pages/invalid_page.py
+++ pages/invalid_page.py
@@ -1,11 +1,73 @@
+import time
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
-#TODO: Create Invalid IOD test module  
+from pages.home_page import DriverHomePage
+
 class InvalidIODPage:
+    invalid_list = []
+    
     def __init__(self, driver: webdriver.Remote):
         self.driver = driver
     
     def nav_invalid(self):
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "Invalid IOD").click()
-        self.driver.back()
+    
+    #NOTE: The main process
+    def load_invalidPage(self):
+        if  self.get_invalidDocket() == True:
+             self.review_pod()
+             self.retake_pod()
+        else:
+            self.driver.back()   
+            
+        DriverHomePage.load_driverHome(self)
+            
+    def get_invalidDocket(self) -> bool:
+        try:
+            #Wait for the element to shows up
+            WebDriverWait(self.driver,20).until(EC.presence_of_element_located((AppiumBy.XPATH, '//android.view.View[starts-with(@content-desc, "JD3")]')))
+
+            #Get the elements
+            all_items = self.driver.find_elements(AppiumBy.XPATH, '//android.view.View[starts-with(@content-desc, "JD3")]')
+            
+            for item in all_items:
+                if item.is_displayed():
+                    docket = item.get_attribute('content-desc')
+                    self.invalid_list.append(docket)
+                                
+            shows = True
+            
+        except TimeoutException:
+            shows = False
+            raise ValueError("TimeoutException: No invalid iod available")
+
+        return shows
+    
+    def review_pod(self):
+        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, self.invalid_list[0]).click()
+        self.driver.find_element(AppiumBy.XPATH, '//android.widget.Button[@content-desc="Preview"]').click()
+        
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.XPATH, '//android.view.View[starts-with(@content-desc, "JD3")]')))
+            time.sleep(1)
+            self.driver.back()
+            
+        except TimeoutException:
+            raise ValueError("TimeoutException: No preview available")
+
+    def retake_pod(self):
+        self.driver.find_element(AppiumBy.XPATH, '//android.widget.Button[@content-desc="Retake"]').click()
+        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Shutter').click()
+        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Done').click()
+        
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Confirm Upload'))).click()
+
+        except TimeoutException:
+            raise ValueError("TimeoutException: Unable to locate [Confirm Upload]")
+    
+        
