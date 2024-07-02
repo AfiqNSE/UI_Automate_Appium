2e9deab4686417e9da149d62007383b0288e87e9
AfiqNSE <afiq@nse.com.my>
2024-07-02 12:19:00 +0800

    update test assign

--- tests/test_assign.py
+++ tests/test_assign.py
@@ -3,7 +3,7 @@ from appium.options.android import UiAutomator2Options
 from appium import webdriver
 
 from config import Config
-from pages.staff_page import AssignPointPage
+from pages.assign_page import AssignPointPage
 
 class TestStaffAssign(unittest.TestCase):
     def setUp(self):
