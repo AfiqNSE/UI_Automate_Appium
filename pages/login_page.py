ccb4798b4256d9849cb448dfce8452460091a5e5
AfiqNSE <afiq@nse.com.my>
2024-07-03 17:08:26 +0800

    minor update for login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,16 @@
+import os
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
+from dotenv import load_dotenv
 
-from components.constant_component import Constant
 
 #Testing class for Login
 class LoginPage:
+    load_dotenv()
+    staff_username = os.getenv("STAFF_USERNAME")
+    driver_username = os.getenv('DRIVER_USERNAME')
+    password = os.getenv("PASSWORD")
+
     #defining constructor  
     def __init__(self, driver: webdriver.Remote):
         self.driver = driver
@@ -13,21 +19,21 @@ class LoginPage:
         textfield = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]')
         textfield.click()
         textfield.clear()
-        textfield.send_keys(Constant.STAFF_USERNAME)
+        textfield.send_keys(self.staff_username)
         self.driver.hide_keyboard()     
         
     def enter_driver_username(self):
         textfield = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[1]')
         textfield.click()
         textfield.clear()
-        textfield.send_keys(Constant.DRIVER_USERNAME)
+        textfield.send_keys(self.driver_username)
         self.driver.hide_keyboard()   
         
     def enter_password(self):
         textfield = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.EditText[2]')
         textfield.click()
         textfield.clear()
-        textfield.send_keys(Constant.PASSWORD)
+        textfield.send_keys(self.password)
         self.driver.hide_keyboard()    
 
     def click_login(self):
