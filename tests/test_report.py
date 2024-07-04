3083ad6e7351b5679af4b872877529f26806a6bd
AfiqNSE <afiq@nse.com.my>
2024-07-04 15:48:35 +0800

    Update test report

--- tests/test_report.py
+++ tests/test_report.py
@@ -1,31 +1,16 @@
 import unittest
 from appium.options.android import UiAutomator2Options
 from appium import webdriver
-
 from config import Config
-from pages.report_page import IODReportPage, GeneralReportPage 
+from pages.report_page import ReportsPage
+
 
 class TestStaffReport(unittest.TestCase):
     def setUp(self):
         options = UiAutomator2Options().load_capabilities(Config.capabilities)
         self.driver = webdriver.Remote(Config.appium_server_url, options=options)
-        self.iod_page = IODReportPage(self.driver)
-        self.general_page = GeneralReportPage(self.driver)
-
-    def test_01_iod_report(self):
-        self.iod_page.nav_report()
-        self.iod_page.check_form()
-        self.iod_page.iod_form_data()
-        self.iod_page.iod_filter_truckNo()
-        self.iod_page.iod_filter_supplierName()
-        self.iod_page.iod_filter_lateDeliveryFor()
-        self.iod_page.iod_filter_zone()
-        self.iod_page.iod_lateDays_orderBy()
-
-    def test_02_general_report(self):
-        self.general_page.change_report()
-        self.iod_page.check_form()
-        self.general_page.general_form_data()
-        self.general_page.general_filter_truckNo()
-        self.iod_page.iod_filter_supplierName()
-        self.general_page.general_filter_jobNo()
+        self.report_page = ReportsPage(self.driver)
+        
+    def test_report(self):
+        self.report_page.iod_report()
+        self.report_page.general_report()
