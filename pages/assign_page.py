0b338bf0e4632ceb45ae388f57573b3760effe06
AfiqNSE <afiq@nse.com.my>
2024-07-04 15:50:41 +0800

    Update assign page

--- pages/assign_page.py
+++ pages/assign_page.py
@@ -9,59 +9,76 @@ from selenium.common.exceptions import TimeoutException
 from components.main_component import Components
 
 
-# Testing part for Assign Point
 class AssignPointPage:
     load_dotenv()
     assign_logsheetNo = os.getenv("ASSIGN_LOGSHEETNO")
-    assign_dockets = []
+    dockets_list = []
 
     #defining constructor  
     def __init__(self, driver: webdriver.Remote):
         self.driver = driver
+        self.component = Components(self.driver)
 
     def nav_assignPoint(self):
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Assign\nPoints').click()
 
     def scan_logsheet(self):
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Scan Logsheet Barcode').click()
+        try:
+            WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Scan Logsheet Barcode'))).click()
+            
+        except TimeoutException:
+            raise ValueError("TimeoutException: Unable to locate [Scan Logsheet Barcode]")
+        
         self.driver.back()
 
     def insert_logsheet(self):
         self.nav_assignPoint()
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Insert Logsheet Number').click()
-        self.driver.find_element(AppiumBy.XPATH, '//android.widget.EditText').send_keys(self.assign_logsheetNo)
-        self.driver.hide_keyboard()
-        Components.submitButton(self)
-
+        
+        try:
+            WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Insert Logsheet Number'))).click()
+            self.driver.find_element(AppiumBy.XPATH, '//android.widget.EditText').send_keys(self.assign_logsheetNo)
+            self.driver.hide_keyboard()
+            
+            if self.assign_logsheetNo != '':
+                self.component.submitButton()
+            else:
+                self.driver.back()
+                
+        except TimeoutException:
+            raise ValueError("TimeoutException: Unable to locate [Scan Logsheet Barcode]")
+ 
     #NOTE: Check the filter functionality
     def check_filter(self):
-        self.get_AssignDockets()
+        self.get_displayedDockets()
+        time.sleep(2)
 
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Not Assign').click()
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Assigned').click()
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Not IOD').click()
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'All').click()
-        
+    
+    #Check select/deselect all button
     def select_mode(self):
-        #Check select/deselect all button
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Not Assign').click()
-        Components(self.driver).nav_selectMode(self.assign_dockets)
-        Components(self.driver).select_all()
-        Components(self.driver).deselect_all()
-        Components(self.driver).exit_selectMode()
+        self.component.nav_selectMode(self.dockets_list)
+        self.component.select_all()
+        # self.component.deselect_all()
+        self.component.exit_selectMode()
         
     def single_assign(self):
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Not Assign').click()
         
-        if self.assign_dockets != []:
-            self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, self.assign_dockets[0]).click()
+        if self.dockets_list != []:
+            self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, self.dockets_list[0]).click()
 
             # Assign docket
-            Components.submitButton()
+            self.component.submitButton()
             self.driver.find_element(AppiumBy.XPATH, '//android.widget.EditText').send_keys('Testing by Appium')
             self.driver.hide_keyboard()
-            Components.submitButton()
+            self.component.submitButton()
             self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Assigned').click()
+            time.sleep(2)
+            
         else:
             raise ValueError("No docket to be assign")
             
@@ -69,29 +86,32 @@ class AssignPointPage:
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Not Assign').click()
         
         #Get latest element after do single assign
-        self.get_AssignDockets()
-        
-        if self.assign_dockets != []: 
-            element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, self.assign_dockets[0])
+        self.get_displayedDockets()
+        time.sleep(2)
+                
+        if self.dockets_list != []:
+            element = self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, self.dockets_list[0])
             self.driver.execute_script('mobile: longClickGesture', {'elementId': element.id})
 
             for i in range(2):
-                self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, self.assign_dockets[i+1]).click()
+                self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, self.dockets_list[i+1]).click()
 
             el = self.driver.find_element(AppiumBy.XPATH, '//android.widget.EditText')
             el.click()
             el.send_keys('Multiple Testing by Appium')
-            time.sleep(1)
             self.driver.hide_keyboard()
-            Components.submitButton()
+            self.component.submitButton()
             self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Assigned').click()
+            time.sleep(2)
 
         else:
             raise ValueError("No docket to be assign")
-            
+        
+        self.driver.back()       
         
     #NOTE: Get latest element that been display at the UI
-    def get_AssignDockets(self):
+    def get_displayedDockets(self):
+        self.dockets_list = []
         #Wait for the element to shows up & store the latest
         try:
             WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.XPATH, '//android.view.View[starts-with(@content-desc, "JD3")]')))
@@ -101,12 +121,11 @@ class AssignPointPage:
             for item in all_items:
                 if item.is_displayed():
                     docketNo = item.get_attribute("content-desc")
-                    if "Haven't Assign Points" in docketNo:
-                        self.assign_dockets.append(docketNo)
-                    else:
-                        pass
+                    self.dockets_list.append(docketNo)
                    
         except TimeoutException:
-            raise ValueError("Timeout: Elements did not appear within the expected time.")
+            raise ValueError("TimeoutException: unable to located [displayed docket]")
+        
+        
 
 
