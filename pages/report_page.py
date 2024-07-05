9cddf14e1413f73cacd02625d30bb86aeee4b5eb
AfiqNSE <afiq@nse.com.my>
2024-07-05 12:59:08 +0800

    Optimize report code

--- pages/report_page.py
+++ pages/report_page.py
@@ -39,8 +39,8 @@ class ReportsPage:
         self.general_form_data()
         self.general_filter_truckNo()
         self.iod_filter_supplierName()
-        self.general_filter_jobNo()
-        time.sleep(2)
+        self.general_filter_job()
+        self.general_filter_est()
         self.driver.back()
         self.homepage.load_staffHome()
     
@@ -52,10 +52,15 @@ class ReportsPage:
             raise ValueError("TimeoutException: unable to located [Report]")
     
     def change_report(self):
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'General Report').click()
-        self.driver.find_element(AppiumBy.XPATH, '(//android.view.View[@content-desc="General Report"])[1]').click()
+        try:
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'General Report'))).click()
+            self.driver.find_element(AppiumBy.XPATH, '(//android.view.View[@content-desc="General Report"])[1]').click()
+                  
+        except TimeoutException:
+            raise ValueError("TimeoutException: unable to located [Report option]")
 
-    #NOTE:Check if search form visible or not
+
+    #Check if search form visible or not
     def check_form(self):
         try:
             WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Submit')))
@@ -63,6 +68,7 @@ class ReportsPage:
         except TimeoutException:
             self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(3)').click()
 
+    #IOD report form
     def iod_form_data(self):
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Show Truck No').click()
 
@@ -70,11 +76,11 @@ class ReportsPage:
 
         time.sleep(5)
 
-    #NOTE:Filter based on Truck No
+    #Filter based on Truck No
     def iod_filter_truckNo(self):
-        self.driver.find_element(AppiumBy.XPATH, '(//android.widget.Button[@content-desc="Truck No"])[1]').click()
-
         try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.XPATH, '(//android.widget.Button[@content-desc="Truck No"])[1]'))).click()
+            
             # Locate the ScrollView container
             scroll_view = self.driver.find_element(AppiumBy.XPATH, '//android.widget.ScrollView')
 
@@ -87,24 +93,26 @@ class ReportsPage:
                     if item.get_attribute('content-desc') != "Show All":
                         truck_number = item.get_attribute('content-desc')
                         truck_numbers.append(truck_number)
-
-        except TimeoutException:
-            raise ValueError("TimeoutException: unable to located [Filter TruckNo]")
+                        
             
-        for i in range(2):
+            for i in range(2):
                 self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, truck_numbers[i]).click()
 
-        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(5)').click()
-        time.sleep(2)
-
-        #NOTE: Reset filter
-        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(5)').click()
-    
-    #NOTE:Filter based on Supplier Name
+            self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(5)').click()
+            time.sleep(2)
+            
+            #NOTE: Reset filter
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(5)'))).click()
+            
+        except TimeoutException:
+            raise ValueError("TimeoutException: unable to located [Filter TruckNo]")
+        
+            
+    #Filter based on Supplier Name
     def iod_filter_supplierName(self):
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Supplier Name').click()
-
         try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Supplier Name'))).click()
+            
             # Locate the ScrollView container
             scroll_view = self.driver.find_element(AppiumBy.XPATH, '//android.widget.ScrollView')
 
@@ -117,56 +125,52 @@ class ReportsPage:
                     if item.get_attribute('content-desc') != "Show All":
                         supplier_Name = item.get_attribute('content-desc')
                         supplier_Names.append(supplier_Name)
+                        
+            for i in range(2):
+                    self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, supplier_Names[i]).click()
+
+            self.driver.back()
+            time.sleep(2)
+
+            #NOTE: Reset filter
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(5)'))).click()
 
         except:
             raise ValueError("TimeoutException: unable to located [Filter SupplierName]")
 
-        for i in range(2):
-                self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, supplier_Names[i]).click()
-
-        self.driver.back()
-        time.sleep(2)
-
-        #NOTE: Reset filter
-        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(5)').click()
-
-    #NOTE:Filter based on Later Delivery For
+    #Filter based on Later Delivery For
     def iod_filter_lateDeliveryFor(self):
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Late Delivery').click()
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Less than 14 Days').click()
-        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(5)').click()
-        time.sleep(2)
-        
-        #NOTE: Reset filter
-        #TODO: Fix reset filter at FE for late delivery
-        # self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(5)').click()
-
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Less than 14 Days').click()
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Show All').click()
-        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(5)').click()
-
-    #NOTE:Filter based on Zone    
-    #TODO: Fix Reset filter at FE at FE for zone
-    def iod_filter_zone(self):
-        el = self.driver.find_element(AppiumBy.XPATH, '//android.widget.Button[@content-desc="Late Delivery"]')
-        
-        actions = ActionChains(self.driver)
-        x = -400
-        y = 0
-        
-        #Tap the late delivery filter and swipe to left
-        actions.move_to_element(el)
-        actions.click_and_hold().move_by_offset(xoffset=x, yoffset=y).release().perform()
-        
         try:
-            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Zone'))).click()
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Late Delivery'))).click()
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Less than 14 Days'))).click()
+            self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(5)').click()
+            time.sleep(2)
+            
+            #Reset back to show all
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Less than 14 Days'))).click()
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Show All'))).click()
+            self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(5)').click()
             
         except TimeoutException:
-            raise ValueError("TimeoutException: unable to located [Filter Zone]")
-        
+            raise ValueError("TimeoutException: unable to located some elemenets in [Filter Late Delivery For]")
+     
+
+    #Filter based on Zone    
+    def iod_filter_zone(self):
         try:
+            #Tap the late delivery filter and swipe to left
+            el = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.XPATH, '//android.widget.Button[@content-desc="Late Delivery"]')))
+            actions = ActionChains(self.driver)
+            x = -400
+            y = 0
+            
+            actions.move_to_element(el)
+            actions.click_and_hold().move_by_offset(xoffset=x, yoffset=y).release().perform()
+            
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Zone'))).click()
+            
             # Locate the ScrollView container
-            scroll_view = self.driver.find_element(AppiumBy.XPATH, '//android.widget.ScrollView')
+            scroll_view  = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.XPATH, '//android.widget.ScrollView')))
 
             # Find all direct child views within the ScrollView
             all_items = scroll_view.find_elements(AppiumBy.XPATH, './/android.view.View')
@@ -177,68 +181,64 @@ class ReportsPage:
                     if item.get_attribute('content-desc') != "Show All":
                         zone = item.get_attribute('content-desc')
                         zone_list.append(zone)
-
-        except:
-            raise ValueError("TimeoutException: unable to located [Zone line]")
-        
-        for i in range(2):
-            self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, zone_list[i]).click()
             
-        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(5)').click()
-        time.sleep(2)
+            for i in range(2):
+                self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, zone_list[i]).click()
+            
+            self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(5)').click()
+            time.sleep(2)
+            
+        except TimeoutException:
+            raise ValueError("TimeoutException: unable to located some elements in [Filter Zone]")
         
+          
     def iod_lateDays_orderBy(self):
-        #NOTE: Multiple taps to show desc & asc order
+        #Multiple taps to show desc & asc order
         for _ in range(3):
             self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Late Days').click()
         
     def general_form_data(self):
-        # Insert Customer name
         try:
+            # Insert Customer name
             customerName = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.EditText").instance(1)')))
             customerName.click()
             customerName.send_keys('DAIKIN')
             self.driver.hide_keyboard()
-
-        except TimeoutException:
-            raise ValueError("TimeoutException: unable to located [Text field]")
-  
-        # Pick Date From
-        try:
+            
+            
+            # Pick Date From
             WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.XPATH, '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[1]'))).click()
             WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(1)')))
             
             for _ in range(5):
                 self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(1)').click()
 
-            self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, self.report_dateFrom).click()
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ACCESSIBILITY_ID, self.report_dateFrom))).click()
             self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'OK').click()
-                
-        except TimeoutException:
-            raise ValueError("TimeoutException: unable to located [Date From]")
-        
-        # Pick Date To
-        try:
+
+            
+            # Pick Date To
             WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.XPATH, '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]'))).click()
             WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ACCESSIBILITY_ID, self.report_dateTo))).click()
             self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'OK').click()
 
+
+            # Click show truck no
+            self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Show Truck No').click()
+            self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Submit').click()
+            time.sleep(5)
+            
         except TimeoutException:
-            raise ValueError("TimeoutException: unable to located [Date To]")
+            raise ValueError("TimeoutException: unable to located some element in [General Form Data]]")
 
-        # Click show truck no
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Show Truck No').click()
 
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Submit').click()
-
-        time.sleep(5)
-
+    #Filter truck no
     def general_filter_truckNo(self):
-        self.driver.find_element(AppiumBy.XPATH, '(//android.widget.Button[@content-desc="Truck No"])[1]').click()
-
         try:
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.XPATH, '(//android.widget.Button[@content-desc="Truck No"])[1]'))).click()
+            
             # Locate the ScrollView container
-            scroll_view = self.driver.find_element(AppiumBy.XPATH, '//android.widget.ScrollView')
+            scroll_view  = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.XPATH, '//android.widget.ScrollView')))
 
             # Find all direct child views within the ScrollView
             all_items = scroll_view.find_elements(AppiumBy.XPATH, './/android.view.View')
@@ -250,26 +250,55 @@ class ReportsPage:
                         truck_number = item.get_attribute('content-desc')
                         truck_numbers.append(truck_number)
 
-        except:
-            raise ValueError("TimeoutException: unable to located [Filter TruckNo]")
+            for i in range(2):
+                    self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, truck_numbers[i]).click()
+
+            self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(5)').click()
+            time.sleep(2)
             
-        for i in range(2):
-                self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, truck_numbers[i]).click()
-
-        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(5)').click()
-        time.sleep(2)
+            #NOTE: Reset filter
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(5)'))).click()
         
-        #NOTE: Reset filter
-        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(5)').click()
+        except:
+            raise ValueError("TimeoutException: unable to located some elements in [Filter TruckNo]")
+            
+            
+    #Filter Job  
+    def general_filter_job(self):
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.XPATH, '(//android.widget.Button[@content-desc="Job %"])[1]'))).click()
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, '60% to 90%'))).click()
 
-    def general_filter_jobNo(self):
-        self.driver.find_element(AppiumBy.XPATH, '(//android.widget.Button[@content-desc="Job %"])[1]').click()
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, '60% to 90%').click()
+            self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(5)').click()
+            time.sleep(2)
 
-        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(5)').click()
-        time.sleep(2)
-
-        #NOTE: Reset filter
-        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(5)').click()
+            #NOTE: Reset filter
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(5)'))).click()
         
-       
+        except TimeoutException:
+            raise ValueError("TimeoutException: unable to located some elements in [Filter Job]")
+
+    
+    #Filter Est
+    def general_filter_est(self):
+        try:
+            #Tap the job filter and swipe to left
+            el = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.XPATH, '(//android.widget.Button[@content-desc="Job %"])[1]')))
+            actions = ActionChains(self.driver)
+            x = -400
+            y = 0
+            
+            actions.move_to_element(el)
+            actions.click_and_hold().move_by_offset(xoffset=x, yoffset=y).release().perform()
+            
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.XPATH, '(//android.widget.Button[@content-desc="Est %"])[1]'))).click()
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ACCESSIBILITY_ID, '60% to 90%'))).click()
+            
+            self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(5)').click()
+            time.sleep(2)
+            
+            #NOTE: Reset filter
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(5)'))).click()
+            
+        except TimeoutException:
+            raise ValueError("TimeoutException: unable to located some elements in [Filter Est]")
