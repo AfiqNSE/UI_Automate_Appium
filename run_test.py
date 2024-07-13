83576b6509d8f69e243de4ab49008122ad6c1137
AfiqNSE <afiq@nse.com.my>
2024-07-13 12:47:35 +0800

    Code structure optimization

--- run_test.py
+++ run_test.py
@@ -5,46 +5,51 @@ from tests.iod.test_approve import TestStaffApprove
 from tests.iod.test_assign import TestStaffAssign
 from tests.iod.test_invalid import TestDriverInvalid
 from tests.iod.test_language import TestStaffLanguage, TestDriverLanguage
-from tests.iod.test_login_logout import  TestDriverLogout, TestStaffLogin, TestDriverLogin, TestStaffLogout
+from tests.iod.test_login_logout import TestIODLoginLogout
 from tests.iod.test_longhaul import TestStaffLonghaul, TestDriverLonghaul
 from tests.iod.test_report import TestStaffReport
 from tests.iod.test_reward import TestDriverReward
 from tests.iod.test_search import TestDriverSearch, TestStaffSearch
 from tests.iod.test_notification import TestNotification
 from tests.iod.test_homepage import TestStaffHome, TestDriverHome
+from tests.oms.test_homepage import TestOMSHomePage
+from tests.oms.test_login_logout import TestOMSLogin
 
 
 if __name__ == '__main__':
     loader = unittest.TestLoader()
-    staff_suite = unittest.TestSuite()
-    driver_suite = unittest.TestSuite()
+    iod_suite = unittest.TestSuite()
+    oms_suite = unittest.TestSuite()
 
-    #Staff Test
-    staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffLogin)) 
-    staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffHome)) 
-    staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffLanguage)) 
-    staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffLonghaul)) 
-    staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffApprove)) 
-    # staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffSearch)) 
-    # staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffAssign)) 
-    staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffAnalytics)) 
-    staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffReport)) 
-    staff_suite.addTests(loader.loadTestsFromTestCase(TestStaffLogout)) 
+    #IOD App Staff Test
+    iod_suite.addTest(TestIODLoginLogout("test_Login", user_type='staff'))
+    iod_suite.addTests(loader.loadTestsFromTestCase(TestStaffHome)) 
+    iod_suite.addTests(loader.loadTestsFromTestCase(TestStaffLanguage)) 
+    iod_suite.addTests(loader.loadTestsFromTestCase(TestStaffLonghaul)) 
+    iod_suite.addTests(loader.loadTestsFromTestCase(TestStaffApprove)) 
+    # iod_suite.addTests(loader.loadTestsFromTestCase(TestStaffSearch)) 
+    # iod_suite.addTests(loader.loadTestsFromTestCase(TestStaffAssign)) 
+    iod_suite.addTests(loader.loadTestsFromTestCase(TestStaffAnalytics)) 
+    iod_suite.addTests(loader.loadTestsFromTestCase(TestStaffReport)) 
+    iod_suite.addTest(TestIODLoginLogout("test_Logout", user_type='staff'))
 
+    #IOD App Driver Test
+    iod_suite.addTest(TestIODLoginLogout("test_Login", user_type='driver'))
+    iod_suite.addTests(loader.loadTestsFromTestCase(TestDriverHome))
+    iod_suite.addTests(loader.loadTestsFromTestCase(TestNotification)) 
+    # iod_suite.addTests(loader.loadTestsFromTestCase(TestDriverLonghaul))
+    # iod_suite.addTests(loader.loadTestsFromTestCase(TestDriverSearch))
+    iod_suite.addTests(loader.loadTestsFromTestCase(TestDriverAnalytics))
+    iod_suite.addTests(loader.loadTestsFromTestCase(TestDriverInvalid))
+    iod_suite.addTests(loader.loadTestsFromTestCase(TestDriverReward))
+    iod_suite.addTest(TestIODLoginLogout("test_Logout", user_type='driver'))
+
+    #OMS App Test
+    oms_suite.addTests(loader.loadTestsFromTestCase(TestOMSLogin))
+    oms_suite.addTests(loader.loadTestsFromTestCase(TestOMSHomePage))
     
-    #Driver Test
-    driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverLogin))
-    driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverHome))
-    driver_suite.addTests(loader.loadTestsFromTestCase(TestNotification)) 
-    # driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverLonghaul))
-    # driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverSearch))
-    driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverAnalytics))
-    driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverInvalid))
-    driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverReward))
-    driver_suite.addTests(loader.loadTestsFromTestCase(TestDriverLogout))
+    # unittest.TextTestRunner(verbosity=Config.VERBOSITY).run(iod_suite)
+    unittest.TextTestRunner(verbosity=Config.VERBOSITY).run(oms_suite)
 
- 
-    unittest.TextTestRunner(verbosity=Config.VERBOSITY).run(staff_suite)
-    unittest.TextTestRunner(verbosity=Config.VERBOSITY).run(driver_suite)
 
 
--- tests/iod/test_login_logout.py
+++ tests/iod/test_login_logout.py
@@ -1,54 +1,61 @@
+import os
 import unittest
 from appium.options.android import UiAutomator2Options
 from appium import webdriver
+from dotenv import load_dotenv
 from components.main_component import Components
 from config import Config
 from pages.iod.home_page import DriverHomePage, StaffHomePage
-from pages.iod.login_page import LoginPage
+from pages.iod.login_page import IODLoginPage
 
-class TestStaffLogin(unittest.TestCase):
+class BaseTest(unittest.TestCase):
+    #Base class for Appium tests. Provides shared setup/teardown and page objects.
+    driver = None
+    login_page = None
+    staff_home = None
+    driver_home = None
+    logout_staff = None
+    logout_driver = None
+
+    @classmethod
+    def setUpClass(cls):
+        #Sets up the Appium driver and page objects for all tests in the class.
+        load_dotenv()
+        options = UiAutomator2Options().load_capabilities(Config.iod_capabilities)
+        cls.driver = webdriver.Remote(Config.appium_server_url, options=options)
+        cls.login_page = IODLoginPage(cls.driver)
+        cls.staff_home = StaffHomePage(cls.driver)
+        cls.driver_home = DriverHomePage(cls.driver)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+class TestIODLoginLogout(BaseTest):
+    def __init__(self, methodName='runTest', user_type=None):
+        super().__init__(methodName)
+        load_dotenv()
+        self.staff_username = os.getenv("STAFF_USERNAME")
+        self.driver_username = os.getenv('DRIVER_USERNAME')
+        self.password = os.getenv("PASSWORD")
+        self.user_type = user_type
+        
     def test_Login(self):
-        options = UiAutomator2Options().load_capabilities(Config.capabilities)
-        self.driver = webdriver.Remote(Config.appium_server_url, options=options)
-        self.login_page = LoginPage(self.driver)
-        self.home_page = StaffHomePage(self.driver)
-        
-        self.login_page.enter_staff_username()
-        self.login_page.enter_password()
+        if self.user_type == 'staff':
+            self.login_page.enter_username(self.staff_username)
+        elif self.user_type == 'driver':
+            self.login_page.enter_username(self.driver_username)
+            
+        self.login_page.enter_password(self.password)
         self.login_page.click_login()
-        self.home_page.load_staffHome()
-
-
-class TestStaffLogout(unittest.TestCase):
-    def test_staffLogout(self):
-        options = UiAutomator2Options().load_capabilities(Config.capabilities)
-        self.driver = webdriver.Remote(Config.appium_server_url, options=options)
-        self.logout_staff = Components(self.driver)
-    
-        self.logout_staff.staff_logout()
-
         
-class TestDriverLogin(unittest.TestCase):
-    def test_Login(self):
-        options = UiAutomator2Options().load_capabilities(Config.capabilities)
-        self.driver = webdriver.Remote(Config.appium_server_url, options=options)
-        self.login_page = LoginPage(self.driver)
-        self.home_page = DriverHomePage(self.driver)
-        
-        self.login_page.enter_driver_username()
-        self.login_page.enter_password()
-        self.login_page.click_login()
-        self.home_page.load_driverHome()
-    
-
-class TestDriverLogout(unittest.TestCase):
-    def test_driverLogout(self):
-        options = UiAutomator2Options().load_capabilities(Config.capabilities)
-        self.driver = webdriver.Remote(Config.appium_server_url, options=options)
-        self.logout_driver = Components(self.driver)
-    
-        self.logout_driver.driver_logout()          
-
-
-
-
+        if self.user_type == 'staff':
+            self.staff_home.load_staffHome()
+        elif self.user_type == 'driver':
+            self.driver_home.load_driverHome()
+            
+    def test_Logout(self):
+        if self.user_type == 'staff':
+            self.staff_home.staff_logout()
+        elif self.user_type == 'driver':
+            self.driver_home.driver_logout()
