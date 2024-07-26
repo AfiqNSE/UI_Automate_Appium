03c54b83e5dff2e11a258dd3e03e6c79b14c383c
AfiqNSE <afiq@nse.com.my>
2024-07-26 17:07:21 +0800

    Update and optimiza langauge

--- pages/iod/language_page.py
+++ pages/iod/language_page.py
@@ -11,9 +11,55 @@ class LanguagePage:
     def __init__(self, driver: webdriver.Remote):
         self.driver = driver
         
-    def nav_staff_language(self):
-        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(1)').click()
+    def nav_changeMalay(self):
+        try:
+            err = self.nav_optionButton()
+            if err != None: return(err)
+            
+            err = self.malay_language()
+            if err != None: return(err)
+            
+            time.sleep(2)
+               
+        except Exception as e:
+            return("Error: Unable to perform langauge changes", e)
+        
+    def nav_changeEnglish(self):
+        try:
+            err = self.nav_optionButton()
+            if err != None: return(err)
+            
+            err = self.english_language()
+            if err != None: return(err)
+            
+            time.sleep(2)
+            
+        except Exception as e:
+            return("Error: Unable to perform langauge changes", e)
+        
+    def nav_optionButton(self) -> str:
+        try:
+            WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.XPATH, '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.Button[2]'))).click()
+        
+        except TimeoutException:
+            return("TimeoutException: Unable to find element [Language button]")
+    
+    def malay_language(self) -> str:
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Bahasa Melayu'))).click()
+            
+        except TimeoutException:
+            self.driver.back()
+            return("TimeoutException: Unable to locate [Malay Language]")
 
+    def english_language(self) -> str:
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'English'))).click()
+            
+        except TimeoutException:
+            self.driver.back()
+            return("TimeoutException: Unable to locate [English Language]")
+        
     def nav_driver_english(self):
         IODComponents.nav_sideBar(self)
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Language').click()
@@ -23,17 +69,3 @@ class LanguagePage:
         IODComponents.nav_sideBar(self)
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Bahasa').click()
         time.sleep(2)
-    
-    #Change to Malay
-    def malay_language(self):
-        try:
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Bahasa Melayu'))).click()
-        except TimeoutException:
-            print("TimeoutException: Unable to locate [Malay Language]")
-
-    #change to English
-    def english_language(self):
-        try:
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'English'))).click()
-        except TimeoutException:
-            print("TimeoutException: Unable to locate [English Language]")
