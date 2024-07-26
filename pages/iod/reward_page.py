56360d7df29a2ff988ecd1d62e4c5990cd2b69a2
AfiqNSE <afiq@nse.com.my>
2024-07-26 17:08:43 +0800

    Optimize reward page

--- pages/iod/reward_page.py
+++ pages/iod/reward_page.py
@@ -11,38 +11,112 @@ from components.iod_component import IODComponents
 class RewardPage:
     def __init__(self, driver: webdriver.Remote):
         self.driver = driver
+        self.component = IODComponents(self.driver)
         
-    def nav_reward(self):
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "Reward").click()
-
-    def load_rewardPage(self):
-        self.redeem_slide()
-        self.redeem_insert()
-        self.driver.back()
-            
-    def check_point(self) -> bool:
+    def nav_reward(self) -> str:
+        err = self.component.nav_sideBar()
+        if err != None:
+            return(err)
+        
         try:
-            WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.XPATH, '//android.view.View[starts-with(@content-desc, "POINT")]')))
-
-            element = self.driver.find_element(AppiumBy.XPATH, '//android.view.View[starts-with(@content-desc, "POINT")]')
-            point = element.get_attribute("content-desc").lstrip('POINT\n')
+            #Navigate to reward page
+            WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, "Reward"))).click()
             
         except TimeoutException:
-            print("TimeoutException: unable to located [Point]")
+            self.driver.back()
+            return("TimeoutException: Unable to locate element [Reward button]")
 
-        if(int(point) > 1000):
-            valid = True
-        else:
-            valid = False
-
-        return valid
-    
-    def redeem_slide(self):
-        if self.check_point() is True:
-            self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "Redeem Points").click()
+    #Driver leaderboard
+    def nav_leaderboard(self) -> str:
+        try:
+            WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Leaderboard'))).click()
+            time.sleep(2)
+           
+            months = self.component.get_month()
+            if months > 0:   
+                #Select first month
+                self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, months[0]).click()
+                time.sleep(2)      
+                self.driver.back()
             
-            slider_element = self.driver.find_element(AppiumBy.XPATH, '//android.widget.SeekBar[@content-desc="0%"]')
-        
+            else:
+                self.driver.back()
+                return("Alert: No months available")
+         
+        except TimeoutException:
+            return("TimeoutException: unable to locate element [Leaderboard button]")
+    
+    #Driver redeem
+    def nav_redeem(self) -> str:
+        valid = self.check_point()
+        if valid == True:
+            try:
+                WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Redeem'))).click()
+                
+                err = self.redeem_slide()
+                if err != None:
+                    return(err)
+                
+                err = self.redeem_insert()
+                if err != None:
+                    return(err)
+                
+                self.driver.back()
+                
+            except TimeoutException:
+                return("TimeoutException: unable to locate element [Redeem button]")
+            
+        else:
+            return('Alert: No enough point to redeem')
+           
+    #Driver points history
+    def nav_history(self) -> str:
+        try:
+            WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'History'))).click()
+            time.sleep(2)
+           
+            try:
+               WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Earn\nTab 2 of 3'))).click()
+               time.sleep(2)
+               
+               WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Redeem\nTab 3 of 3'))).click()
+               time.sleep(2)
+               
+               WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'All\nTab 1 of 3'))).click()
+               time.sleep(2)
+               
+               self.driver.back()
+               
+            except TimeoutException:
+                self.driver.back()
+                return("TimeoutException: unable to locate element [All/Earn/Redeem button]")
+           
+        except TimeoutException:
+            return("TimeoutException: unable to locate element [History button]")
+    
+    #Check the driver points
+    def check_point(self):
+        try:
+            WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.XPATH, '//android.view.View[starts-with(@content-desc, "Points")]')))
+
+            element = self.driver.find_element(AppiumBy.XPATH, '//android.view.View[starts-with(@content-desc, "Points")]')
+            point = element.get_attribute("content-desc").lstrip('Points: \n')
+            
+            print(point)
+            if(int(point) > 1000):
+                return True
+            else:
+                return False
+            
+        except TimeoutException:
+            self.driver.back()
+            return("TimeoutException: unable to locate element [Points]")
+    
+    #Redeem by using slide
+    def redeem_slide(self) -> str:
+        try:
+            slider_element = WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.XPATH, '//android.widget.SeekBar[@content-desc="0%"]')))
+            
             actions = ActionChains(self.driver)
             x = 1
             y = 1490
@@ -52,40 +126,28 @@ class RewardPage:
             actions.move_by_offset(xoffset=x, yoffset=y).release().perform()
             
             self.redeem_button()
-            time.sleep(1)
             self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Dismiss').click()
-            self.driver.back()
-            self.refresh_button()
             time.sleep(2)
             
-        else:
-            print("Not enough points to redeem")
-            pass
-            
-    def redeem_insert(self):
-        if self.check_point() is True:
-            self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "Redeem Points").click()
-            
-            self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'ENTER AMOUNT').click()
+        except TimeoutException:
+            return("TimeoutException: unable to locate element [Slide]")
+    
+    #Redeem by insert number        
+    def redeem_insert(self) -> str:
+        try:
+            WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'ENTER AMOUNT'))).click()
             self.driver.find_element(AppiumBy.XPATH, '//android.widget.EditText').send_keys('500')        
             IODComponents.submitButton(self)
                 
             self.redeem_button()
-            time.sleep(1)
             self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Dismiss').click()
-            self.driver.back()
-            self.refresh_button()
             time.sleep(2)
             
-        else:
-            print("Not enough points to redeem")
-            pass
+        except TimeoutException:
+            return("TimeoutException: unable to locate element [Enter Amount]")
               
     def redeem_button(self):
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'REDEEM').click()
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'REDEEM').click()
-        
-    def refresh_button(self):
-        self.driver.find_element(AppiumBy.XPATH, '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.Button[2]').click()
 
         
