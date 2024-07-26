d9934cf4611f4b5bc2417972030d73135a01338d
AfiqNSE <afiq@nse.com.my>
2024-07-26 17:08:06 +0800

    Update and optimize longhaul

--- pages/iod/longhaul_page.py
+++ pages/iod/longhaul_page.py
@@ -7,126 +7,149 @@ from components.iod_component import IODComponents
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-
 from pages.iod.home_page import DriverHomePage
 
 
 class LonghaulPage:
-    load_dotenv()
-    pod_longhaulNo = os.getenv("POD_LONGHAULNO")
-    fail_longhaulNo = os.getenv("FAIL_LONGHAULNO")
-    
-    longhaul_list = []
-    
     #defining constructor  
     def __init__(self, driver: webdriver.Remote):
+        load_dotenv()
+        self.pod_longhaulNo = os.getenv("POD_LONGHAULNO")
+        self.fail_longhaulNo = os.getenv("FAIL_LONGHAULNO")
         self.driver = driver
         self.component = IODComponents(self.driver)
-    
-    #Main process driver longhaul    
-    def load_longhaulPage(self):
-        self.component.nav_sideBar()
-        self.nav_driver_longhaul()
+        self.longhaul_list = []
         
-        if self.get_displayedLonghaul() is True:
-            self.driver_longhaul_pod()
-            self.driver_longhaul_fail()
-            self.longhaul_viewDetails()
+    #Main process for longhaul acceptance
+    def nav_staff_longhaul(self) -> str:
+        try:
+            WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Longhaul\nAcceptance'))).click()
+            time.sleep(2)
+            self.driver.back()
+            time.sleep(2)
             
-            for _ in range(2):
-                self.driver.back()   
-            
-        else:
-            self.driver.back()   
-
-    def driver_longhaul_pod(self):
-        self.insert_longhaul(self.pod_longhaulNo)
-        self.pod_longhaul()
-        DriverHomePage.load_driverHome(self)
-        time.sleep(2)
-              
-    def driver_longhaul_fail(self):
-        self.component.nav_sideBar()
-        self.nav_driver_longhaul()
-        self.insert_longhaul(self.fail_longhaulNo)      
-        self.component.nav_fail()
-        DriverHomePage.load_driverHome(self)
-        time.sleep(2)
-
-    def longhaul_viewDetails(self):
-        self.component.nav_sideBar()
-        self.nav_driver_longhaul()
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, self.longhaul_list[0]))).click()
-        time.sleep(2)
-
-    #Longhaul navigation for staff
-    def nav_staff_longhaul(self):
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Longhaul\nAcceptance').click()
-        self.component.cancelButton()
-        time.sleep(2)
-    
-    #Longhaul navigation for driver
-    def nav_driver_longhaul(self):
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Longhaul').click()
+        except TimeoutException:
+            return("TimeoutException: Unable to locate element [Loghaul Acceptance button]")
+      
+    def nav_driver_longhaul(self) -> str:
+        err = self.component.nav_sideBar()
+        if err != None: return err
         
         try:
-            WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Longhaul')))
-                      
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Longhaul'))).click()
+            
+            #Longhaul page
+            WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Longhaul')))
+                    
         except TimeoutException:
-            raise ValueError('TimeoutException: Unable to locate [longhaul page]')
-
-    def option_button(self):
-        try:
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Scan/Insert Loghaul Number'))).click()
-        
-        except TimeoutException:
-            raise ValueError('TimeoutException: Unable to locate [option button]')
+            return('TimeoutException: Unable to locate [longhaul page]')
         
     def scan_longhaul(self):
-        self.option_button()
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Scan Longhaul Barcode').click()
-        self.component.cancelButton()
-
-                
-    def insert_longhaul(self, longhaulNo):
-        self.option_button()
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Insert Longhaul Number').click()
-        self.driver.find_element(AppiumBy.XPATH, '//android.widget.EditText').send_keys(longhaulNo)
-        self.driver.hide_keyboard()
-        
-        if longhaulNo != '':
-            self.component.submitButton()
-        
-        else:
-            for _ in range(2):
-                self.driver.back()  
-                 
-            raise ValueError('\nNo longhaul number provided')
-    
-    def get_displayedLonghaul(self) -> bool:
         try:
-            WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.XPATH, '//android.view.View[starts-with(@content-desc, "LH3")]')))
-
-            all_items = self.driver.find_elements(AppiumBy.XPATH, '//android.view.View[starts-with(@content-desc, "LH3")]')
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Scan/Insert Loghaul Number'))).click()
             
-            for item in all_items:
-                if item.is_displayed():
-                    longhaul = item.get_attribute('content-desc')
-                    self.longhaul_list.append(longhaul)
-                    
-            if len(self.longhaul_list) > 0:
-                return True
+            try:
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Scan Longhaul Barcode'))).click()
+                
+                self.driver.back()
+                
+            except TimeoutException:
+                self.driver.back()
+                return("TimeoutException: Unable to locate element [Scan Longhaul Barcode]")
+        
+        except TimeoutException:
+            return('TimeoutException: Unable to locate [option button]')
+    
+    #View longhaul
+    def longhaul_viewDetails(self) -> str:
+        try:
+            longhaul = self.get_displayedLonghaul()
+            
+            if len(longhaul) > 0:
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, longhaul[0]))).click()
+                time.sleep(2)
+                
+                for _ in range(2):
+                    self.driver.back()
+            
             else:
-                return False
+                self.driver.back()
+                return("Error: Empty longhaul")
+                
+        except TimeoutException:
+            return("TimeoutException: Unable to open selected longhaul")
+        
+    #POD longhaul   
+    def driver_longhaul_pod(self) -> str:
+        errNo = self.insert_longhaulNo(self.pod_longhaulNo)
+        if errNo is None:
+            
+            errPOD = self.nav_process()
+            if errPOD is not None: return errPOD
+            
+            DriverHomePage.load_driverHome(self)
+            time.sleep(2)
+            
+        else: return errNo
+    
+    #FAIL longhaul          
+    def driver_longhaul_fail(self) -> str:
+        errNav = self.nav_driver_longhaul()
+        if errNav is not None: return errNav
+        
+        errNo = self.insert_longhaulNo(self.fail_longhaulNo)
+        if errNo is None:
+            
+            errFail = self.component.nav_fail()
+            if errFail is not None: 
+                self.driver.back()
+                return errFail
+            
+            DriverHomePage.load_driverHome(self)
+            time.sleep(2)
+            
+        else: return errNo
+    
+    def insert_longhaulNo(self, longhaulNo) -> str:
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Scan/Insert Loghaul Number'))).click()
+            
+            try:
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Insert Longhaul Number'))).click()
+                
+                self.driver.find_element(AppiumBy.XPATH, '//android.widget.EditText').send_keys(longhaulNo)
+                self.driver.hide_keyboard()
+                self.component.submitButton()
+                
+            except TimeoutException:
+                return('TimeoutException: Unable to locate [Insert longhaul button]')
+        
+        except TimeoutException:
+            return('TimeoutException: Unable to locate [option button]')
+           
+    def nav_process(self) -> str:
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'POD'))).click()
+            
+            err = self.pod_process()
+            if err is None:
+                self.component.submitButton()
+                
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.XPATH, '//android.widget.Button[@content-desc="OK"]'))).click()
+                
+            else:
+                for _ in range(2):
+                    self.driver.back()
+                    
+                return err
             
         except TimeoutException:
-            raise ValueError('TimeoutException: Unable to locate [displayed longhaul]')
+            self.driver.back()
+            return('TimeoutException: Unable to locate [POD button]')
         
-    def pod_longhaul(self):
-        self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'POD').click()
-        
-        #Add DO/logsheet photo
+    def pod_process(self) -> str:
         try:
+            #Add DO/logsheet photo
             WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.XPATH, '(//android.widget.Button[@content-desc="Add Photos"])[1]'))).click()
             self.driver.find_element(AppiumBy.XPATH, '//android.view.View[@content-desc="Take Photo"]').click()
             
@@ -135,29 +158,43 @@ class LonghaulPage:
                 self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Done').click()
                 
             except TimeoutException:
-                raise ValueError("TimeoutException: Unable to locate [Shutter icon]")
-    
-        except TimeoutException:
-            raise ValueError('TimeoutException: Unable to locate [Add photos]')
-        
-        time.sleep(1)
-        
-        #Add attachment
-        try:
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.XPATH, '(//android.widget.Button[@content-desc="Add Photos"])[2]'))).click()
-            self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Open Gallery').click()
+                return("TimeoutException: Unable to locate [Shutter icon]")
             
+            time.sleep(2)
+            
+            #Add attachment
             try:
-                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.XPATH, '//android.widget.LinearLayout[@content-desc="IMG_20240626_034459.jpg, 146 kB, Jun 26"]'))).click()
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.XPATH, '(//android.widget.Button[@content-desc="Add Photos"])[2]'))).click()
+                self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Open Gallery').click()
+                
+                try:
+                    WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, '1721381865966.jpg, 53.40 kB, Jul 19'))).click()
+                    
+                except TimeoutException:
+                    
+                    for _ in range(2):
+                        self.driver.back()
+                        
+                    return("TimeoutException: unable to located [Image from gallery]")
                 
             except TimeoutException:
-                raise ValueError("TimeoutException: unable to located [Image from gallery]")
-            
+                return('TimeoutException: Unable to locate [Add photos]')
+    
         except TimeoutException:
-            raise ValueError('TimeoutException: Unable to locate [Add photos]')
+            return('TimeoutException: Unable to locate [Add photos]')
         
-        time.sleep(1)
+    def get_displayedLonghaul(self):
+        try:
+            WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.XPATH, '//android.view.View[starts-with(@content-desc, "LH3")]')))
+
+            all_items = self.driver.find_elements(AppiumBy.XPATH, '//android.view.View[starts-with(@content-desc, "LH3")]')
+            
+            longhaul_list = []
+            for item in all_items:
+                if item.is_displayed():
+                    longhaul_list.append(item.get_attribute('content-desc'))
+            
+            return longhaul_list
         
-        self.component.submitButton()
-        
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.XPATH, '//android.widget.Button[@content-desc="OK"]'))).click()
+        except TimeoutException:
+            return []
--- tests/iod/test_longhaul.py
+++ tests/iod/test_longhaul.py
@@ -5,23 +5,56 @@ from appium.options.android import UiAutomator2Options
 from pages.iod.longhaul_page import LonghaulPage
 
 
-class TestStaffLonghaul(unittest.TestCase):
+class TestLonghaulAcceptance(unittest.TestCase):
     def setUp(self):
         options = UiAutomator2Options().load_capabilities(Config.iod_capabilities)
         self.driver = webdriver.Remote(Config.appium_server_url, options=options)
         self.longhaul_staff = LonghaulPage(self.driver)
 
     def test_longhaul(self):
-        self.longhaul_staff.nav_staff_longhaul()
+        err = self.longhaul_staff.nav_staff_longhaul()
+        if err != None: print("\nError/Alert Detected:", err)
         
-
+    def tearDown(self):
+        self.driver.quit()
+        
+#Fix the code
 class TestDriverLonghaul(unittest.TestCase):
     def setUp(self):
         options = UiAutomator2Options().load_capabilities(Config.iod_capabilities)
         self.driver = webdriver.Remote(Config.appium_server_url, options=options)
         self.longhaul_driver = LonghaulPage(self.driver)
-
+        self.errorList = []
+        
     def test_longhaul(self):
-        self.longhaul_driver.load_longhaulPage()
+        err = self.longhaul_driver.nav_driver_longhaul()
+        if err != None:
+            self.errorList.append(err)
+        
+        err = self.longhaul_driver.scan_longhaul()
+        if err != None:
+            self.errorList.append(err)
+            
+        err = self.longhaul_driver.longhaul_viewDetails()
+        if err != None:
+            self.errorList.append(err)
+            
+        #Check error list
+        if len(self.errorList) > 0:
+            print("\n [", len(self.errorList), "] Error/Alert Detected:")
+            for error in self.errorList:
+                print(error)
+        
+    def test_longhaulPOD(self):
+        err = self.longhaul_driver.driver_longhaul_pod()
+        if err != None: print("\nError/Alert Detected:", err)
+    
+    def test_longhaulFail(self):
+        err = self.longhaul_driver.driver_longhaul_fail()
+        if err != None: print("\nError/Alert Detected:", err)
+    
+        
+    def tearDown(self):
+        self.driver.quit()
       
       
