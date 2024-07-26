94764c3de19a772dc72794467078de07a2b8cd7f
AfiqNSE <afiq@nse.com.my>
2024-07-26 17:08:26 +0800

    Update and optimize home page

--- pages/iod/home_page.py
+++ pages/iod/home_page.py
@@ -12,27 +12,27 @@ class StaffHomePage:
     def __init__(self, driver: webdriver.Remote):
         self.driver = driver
         
-    def load_staffHome(self):
+    def load_staffHome(self) -> str:
         try:
             WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Management Dashboard')))
         
         except TimeoutException:
-            raise ValueError("TimeoutException: Unable to locate element [management dashboard]")
+            return("TimeoutException: Unable to locate element [management dashboard]")
     
-    def staff_logout(self):
+    def staff_logout(self) -> str:
         try:
             WebDriverWait(self.driver,10).until(EC.presence_of_element_located((AppiumBy.XPATH, '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.Button[3]'))).click()
             time.sleep(2)
             
         except TimeoutException:
-            print("TimeoutException: Unable to locate [Logout button]")
+            return("TimeoutException: Unable to locate [Logout button]")
                  
 
 class DriverHomePage:
-    load_dotenv()
-    view_docketNo = os.getenv("VIEW_DOCKET")
-    
     def __init__(self, driver: webdriver.Remote):
+        load_dotenv()
+        self.present_docket = os.getenv("PRESENT_DOCKET")
+        self.past_docket = os.getenv("PAST_DOCKET")
         self.driver = driver
         self.component = IODComponents(self.driver)
 
@@ -41,73 +41,119 @@ class DriverHomePage:
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Logout').click()
         time.sleep(2)
     
-    def load_driverHome(self):
+    def load_driverHome(self) -> str:
         try:
             WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Home')))
                     
         except TimeoutException:
-            raise ValueError("TimeoutException: Unable to locate [Home Appbar]")
+            return("TimeoutException: Unable to locate [Home Appbar]")
         
-    def logsheet_change(self):
+    def logsheet_change(self) -> str:
         try:
             WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(3)'))).click()
             self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(2)').click()
             time.sleep(2)
             
         except TimeoutException:
-            raise ValueError("TimeoutException: Unable to locate [Logsheet Change Icon]")
+            return("TimeoutException: Unable to locate [Logsheet Change Icon]")
     
-    def option_button(self):
+    def scan_docket(self) -> str:
+        try:
+            err = self.option_button()
+            if err != None: return(err)
+            
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Scan Docket Barcode'))).click()
+            time.sleep(2)
+            self.driver.back()
+        
+        except TimeoutException:
+            self.driver.back()
+            return("TimeoutException: Unable to locate [Scan Docket Barcode Option]")
+    
+    def find_presentDocket(self) -> str:
+        err = self.option_button()
+        if err is None: 
+            
+            try:
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Insert Docket Number'))).click()
+                self.driver.find_element(AppiumBy.XPATH, '//android.widget.EditText').send_keys(self.present_docket)
+                self.driver.hide_keyboard()
+                self.component.submitButton()
+                time.sleep(2)
+                
+                #Go back to the main page
+                self.driver.back()
+                time.sleep(2)
+                
+            except TimeoutException:
+                return("TimeoutException: Unable to locate [Insert Docket Number, present]")
+            
+        else: return(err)
+    
+    def find_pastDocket(self) -> str:
+        err = self.option_button()
+        if err is None: 
+            
+            try:
+                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Insert Docket Number'))).click()
+                self.driver.find_element(AppiumBy.XPATH, '//android.widget.EditText').send_keys(self.past_docket)
+                self.driver.hide_keyboard()
+                self.component.submitButton()
+                time.sleep(2)
+                
+                #Go back to the main page
+                for _ in range(2):
+                    self.driver.back()
+                time.sleep(2)
+                
+            except TimeoutException:
+                return("TimeoutException: Unable to locate [Insert Docket Number, past]")
+            
+        else: return(err)
+
+    def scan_logsheet(self) -> str:
+        try:
+            err = self.option_button()
+            if err != None: return(err)
+            
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Scan Logsheet Barcode'))).click()
+            time.sleep(2)
+            
+            #Go back to the main page
+            self.driver.back()
+            time.sleep(2)
+        
+        except TimeoutException:
+            self.driver.back()
+            return("TimeoutException: Unable to locate [Scan Logsheet Barcode Option]")
+    
+    def scan_DO(self) -> str:
+        try:
+            err = self.option_button()
+            if err != None: return(err)
+            
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Scan Customer DO Barcode'))).click()
+            time.sleep(2)
+            
+            #Go back to the main page
+            self.driver.back()
+            time.sleep(2)
+        
+        except TimeoutException:
+            self.driver.back()
+            return("TimeoutException: Unable to locate [Scan Customer DO Barcode Option]")
+    
+    def option_button(self) -> str:
         try:
             WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Scan Docket/Logsheet Number'))).click()
         
         except TimeoutException:
-            raise ValueError("TimeoutException: Unable to locate [Option Button]")
-        
-    def scan_docket(self):
-        try:
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Scan Docket Barcode'))).click()
-            self.component.cancelButton()
-        
-        except TimeoutException:
-            raise ValueError("TimeoutException: Unable to locate [Scan Docket Barcode Option]")
-    
-    def insert_docket(self):
-        try:
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Insert Docket Number'))).click()
-            self.driver.find_element(AppiumBy.XPATH, '//android.widget.EditText').send_keys(self.view_docketNo)
-            
-            if self.view_docketNo != '':
-                self.component.submitButton()
-            else:
-                raise ValueError('\nNo logsheet number provided')
-            
-            self.driver.back()
-            time.sleep(2)
-            
-        except TimeoutException:
-            raise ValueError("TimeoutException: Unable to locate [Insert Docket Number Option]")
-    
-    def scan_logsheet(self):
-        try:
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Scan Logsheet Barcode'))).click()
-            self.component.cancelButton()
-        
-        except TimeoutException:
-            raise ValueError("TimeoutException: Unable to locate [Scan Logsheet Barcode Option]")
-    
-    def scan_DO(self):
-        try:
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Scan Customer DO Barcode'))).click()
-            self.component.cancelButton()
-        
-        except TimeoutException:
-            raise ValueError("TimeoutException: Unable to locate [Scan Customer DO Barcode Option]")
-        
+            return("TimeoutException: Unable to locate [Option Button]")
+       
     def driverPage_est(self):
         self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.Button").instance(2)').click()
 
-        self.component.get_dockets()
+        self.component.get_new_dockets()
         self.component.nav_estDateTime()
         
         time.sleep(2)
--- tests/iod/test_homepage.py
+++ tests/iod/test_homepage.py
@@ -2,33 +2,32 @@ import unittest
 from appium.options.android import UiAutomator2Options
 from appium import webdriver
 from config import Config
-from pages.iod.home_page import DriverHomePage, StaffHomePage
-
-
-class TestStaffHome(unittest.TestCase):
-    def setUp(self):
-        options = UiAutomator2Options().load_capabilities(Config.iod_capabilities)
-        self.driver = webdriver.Remote(Config.appium_server_url, options=options)
-        self.home_staff = StaffHomePage(self.driver)
-    
-    def test_staffHome(self):   
-        self.home_staff.load_staffHome()
-
-
+from pages.iod.home_page import DriverHomePage
 
 class TestDriverHome(unittest.TestCase):
     def setUp(self):
         options = UiAutomator2Options().load_capabilities(Config.iod_capabilities)
         self.driver = webdriver.Remote(Config.appium_server_url, options=options)
-        self.home_driver = DriverHomePage(self.driver)  
+        self.home_driver = DriverHomePage(self.driver)
+        self.errorList = []
              
     def test_driverHome(self):
-        self.home_driver.load_driverHome()
-        self.home_driver.logsheet_change()
-        self.home_driver.option_button()
-        self.home_driver.insert_docket()
-        #K.I.V, already test at search
-        # self.home_driver.driverPage_est()
+        err = self.home_driver.logsheet_change()
+        if err != None:
+            self.errorList.append(err)
+            
+        err = self.home_driver.find_presentDocket()
+        if err != None:
+            self.errorList.append(err)
+            
+        err = self.home_driver.find_pastDocket()
+        if err != None:
+            self.errorList.append(err)
+        
+        if len(self.errorList) > 0:
+            print("\n [", len(self.errorList), "] Error/Alert Detected:")
+            for error in self.errorList:
+                print(error)
                 
         
 
